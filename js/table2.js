34c6bb766295ec31428bd0e4265d474541432043
kernozz <kernozz@yandex.ru>
2017-02-03 00:48:54 +0200

    Табличка, с одним исключением из двух

--- js/table2.js
+++ js/table2.js
@@ -1,17 +1,29 @@
 function table() {
-	$('#table').html('<table class=energy> ')
-	$('#table').append(' <tr> <td> Найменування </td>    <td> Січень </td>  <td>Лютий</td> <td>Березень</td> <td>Квітень</td><td>Травень</td><td>Червень</td> <td>Липень</td> <td>Серпень </td>  <td>Вересень</td> <td>++Жовтень</td> <td>Листопад</td><td>Грудень</td><td>Всього за рік</td></tr>' ) 
+	$('#table').html(' <table id="table" class="table table-striped table-hover table-bordered">  <tr> <td> Найменування </td>    <td> Січень </td>  <td>Лютий</td> <td>Березень</td> <td>Квітень</td><td>Травень</td><td>Червень</td> <td>Липень</td> <td>Серпень </td>  <td>Вересень</td> <td>Жовтень</td> <td>Листопад</td><td>Грудень</td><td>Всього за рік</td></tr>' ) 
 	function DNZ(){
 		//$('#table').append('<tr><td>"Всього аппарату"</td><td>"sum 01"</td><td>"sum 02"</td><td>"sum 03"</td><td>"sum 04"</td>	<td>"sum 10"</td><td>"sum 11"</td><td>"sum sum1=sum(2,3,4,5,6,7)"</td><td>"sum 01++"</td><td>"sum 02++"</td><td>"sum 03++"</td><td>"sum 04++"</td><td>"sum 10++"</td><td>"sum 11++"</td><td>"sum +sum(8,9,10,11,12,13)+"</td><td>"11"*100/"11++"-100</td>	<td>sum2-sum1</td><td>sum2*100</td>	<td></td></tr> ') 
 		var idobj = 1;
 		var id = 0;
-		var value= "teplo"
+		var value=$('input[name="optradio1"]:checked').val();
 		
+		switch (value) {
+		  case "Електрична енергія":
+		    value='electro';
+		    break
+		    case "Природний газ":
+		    value='gas';
+		    break
+		    case "Водопостачання":
+		    value='water';
+		    break;
+		    case "Витрати тепла":
+		    value='teplo';
+		    break;
+			}	
 		var startDate=$('#date option:selected').text();
 		switch (startDate) {
 			case "2010":
 			startDate = "2010-01-01" ;
-			
 			break
 			case "2011":
 			startDate = "2011-01-01";
@@ -47,19 +59,19 @@ function table() {
 			endDate = "2013-01-01";		
 			break
 			case "2013":
-			endDate = "2014-01-01";			
+			endDate = "2014-01-01";
 			break
 			case "2014":
-			endDate = "2015-01-01";			
+			endDate = "2015-01-01";
 			break
 			case "2015":
-			endDate = "2016-01-01";			
+			endDate = "2016-01-01";
 			break
 			case "2016":
-			endDate = "2017-01-01";			
+			endDate = "2017-01-01";
 			break
 			case "2017":
-			endDate = "2018-01-01";			
+			endDate = "2018-01-01";
 			break;
 		}
 		var data_name = {
@@ -78,39 +90,57 @@ function table() {
 			var names = JSON.parse(names);
 			var arr_ = names.rows.length;
 			console.log(arr_)
-			
-			var ch = 0
-		for (i=0; i<=arr_; i++) {
-		var arr = names.rows[i][0]
-		id = i
+			var arr = names.rows
+			var ch = -1
+		for (var i=1; i<=arr_; i++) {
+
+		var arr_ch = arr
 		var data = {
-			"id":id,
+			"id":i,
 			"param":value,
 					data: [
 								["date",">=",startDate],
 								["date","<=", endDate],
 						  ]	}
-			var requery=JSON.stringify(data)
-		
+		var requery=JSON.stringify(data)
+		console.log(i)
 		//из chart1 забираем значения
 			var values = $.ajax({
 				url: '/myapi/chart1',
 				type: 'post',
+				async: false,
 				data:'data='+ requery,
 				success: function(values) {
 			var values = JSON.parse(values);
-			
 			var arr_1 = values.rows;
-			
-				console.log(arr)
+			ch = ch+1			
+			console.log(i)
 
-				var sum = arr_1[0][1] + arr_1[1][1] + arr_1[2][1] + arr_1[3][1] + arr_1[4][1] + arr_1[5][1] + arr_1[6][1]
-		$('#table').append('<tr><td>'+names.rows[id][0]+'</td><td>'+arr_1[0][1]+'</td><td>'+arr_1[1][1]+'</td><td>'+arr_1[2][1]+'</td><td>'+arr_1[3][1]+'</td>	<td> 0 </td><td>0</td><td>0</td><td> 0 </td><td> 0 </td><td>'+arr_1[4][1]+'</td><td>'+arr_1[5][1]+'</td><td>'+arr_1[6][1]+'</td><td>'+sum.toFixed(2)+'</td></tr> ')
+			if(arr_1 === null){
+				console.log("zz")
+			}
+			else{
+			
+
+			if(value=='teplo')
+				{			
+		var sum = parseInt(arr_1[0][1] + arr_1[1][1] + arr_1[2][1] + arr_1[3][1] + arr_1[4][1] + arr_1[5][1] + arr_1[6][1])
+		$('#table').append('<tr><td>'+arr[ch][0]+'</td><td>'+arr_1[0][1]+'</td><td>'+arr_1[1][1]+'</td><td>'+arr_1[2][1]+'</td><td>'+arr_1[3][1]+'</td>	<td>  0 </td><td>0</td><td>0</td><td> 0 </td><td> 0 </td><td>'+arr_1[4][1]+'</td><td>'+arr_1[5][1]+'</td><td>'+arr_1[6][1]+'</td><td>'+sum.toFixed(2)+'</td></tr> ')
+		}
+		else{
+		var sum = parseInt(arr_1[0][1] + arr_1[1][1] + arr_1[2][1] + arr_1[3][1] + arr_1[4][1] + arr_1[5][1] + arr_1[6][1]+ arr_1[7][1]+ arr_1[8][1]+ arr_1[9][1]+ arr_1[10][1]+ arr_1[11][1])
+		$('#table').append('<tr><td>'+arr[ch][0]+'</td><td>'+arr_1[0][1]+'</td><td>'+arr_1[1][1]+'</td><td>'+arr_1[2][1]+'</td><td>'+arr_1[3][1]+'</td>	<td>'+arr_1[4][1]+'</td><td>'+arr_1[5][1]+'</td><td>'+arr_1[6][1]+'</td><td>'+arr_1[7][1]+'</td><td>'+arr_1[8][1]+'</td><td>'+arr_1[9][1]+'</td><td>'+arr_1[10][1]+'</td><td>'+arr_1[11][1]+'</td><td>'+sum.toFixed(2)+'</td></tr> ')
+		}
 	}
-				})
+
+
+
+		}
+	})
 			}
 				}
 		})
 	}
+
 DNZ()
 }
